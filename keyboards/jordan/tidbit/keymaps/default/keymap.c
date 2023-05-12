ac49e212bc9815d6be6066b6f6e8437c8da899e3
Jordan Munroe <bojan4464@gmail.com>
2023-05-12 10:37:54 -0500

    bunch of things, trying to get oled to work

--- keyboards/jordan/tidbit/keymaps/default/keymap.c
+++ keyboards/jordan/tidbit/keymaps/default/keymap.c
@@ -14,22 +14,27 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "print.h"
 #include <stdio.h>
 #include "jordan/tidbit/tidbit.h"
 #include "jordan/tidbit/config.h"
 #include "bongo.h"
-
 #define KEYBOARD_CENTER_COL 4
 
+uint16_t click_timer = 0;
+bool enc_pressed = false;
 enum layers
 {
     _BASE = 0,
     _FUNC
 };
+enum custom_keycodes {
+    QMK_ENC = SAFE_RANGE,
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-        KC_PSLS, KC_PAST, KC_PMNS,
+        QMK_ENC, KC_PAST, KC_PMNS,
         KC_VOLD, KC_VOLU, KC_P7, KC_P8, KC_P9, KC_PPLS,
         KC_MPRV, KC_MNXT, KC_P4, KC_P5, KC_P6, KC_PPLS,
         KC_LEFT, KC_RGHT, KC_P1, KC_P2, KC_P3, KC_PENT,
@@ -43,16 +48,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ___, ___, ___, ___, ___, ___),
 };
 
+bool oled_task_user(void)
+{
+    rgblight_setrgb(0, 255, 0);
+    draw_bongo(false);
+
+    return false;
+}
+
 oled_rotation_t oled_init_user(oled_rotation_t rotation)
 {
+    rgblight_setrgb(255, 0, 255);
     oled_set_brightness(255);
+    oled_on();
     return OLED_ROTATION_180;
 }
 
-void keyboard_post_init_user(void)
-{
-    rgblight_setrgb(110, 0, 110);
-}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
 {
@@ -64,12 +75,71 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
         bongo_tap(dir);
     }
 
+    if(keycode == QMK_ENC)
+    {
+        enc_pressed = record->event.pressed;
+        if (enc_pressed)
+        {
+            click_timer = timer_read();
+            return false;
+        }
+        else if(timer_elapsed(click_timer) < 500)
+        {
+            rgblight_step();
+            return false;
+        }
+    }
+
     return true;
 }
 
-bool oled_task_user(void)
-{
-    draw_bongo(false);
 
-    return false;
+void keyboard_post_init_user(void)
+{
+  debug_enable=true;
+  debug_matrix=true;
+    //rgblight_setrgb(110, 0, 110);
+}
+
+bool encoder_update_user(uint8_t index, bool clockwise)
+{
+    if(index == 0)
+    {
+        dprintf("clockwise: %s\n", clockwise == true ? "yes" : "no");
+        dprintf("enc_pressed: %s\n", enc_pressed == true ? "yes" : "no");
+        int hue = rgblight_get_hue();
+        int sat = rgblight_get_sat();
+
+        if (clockwise)
+        {
+            if(enc_pressed)
+            {
+                sat += 3;
+            }
+            else
+            {
+                hue += 1;
+            }
+        }
+        else
+        {
+            if(enc_pressed)
+            {
+                sat -= 3;
+            }
+            else
+            {
+                hue -= 1;
+            }
+        }
+        if(hue < 0) hue += 360;
+        if(hue > 360) hue -= 360;
+        if(sat < 0) sat = 0;
+        if(sat > 255) sat = 255;
+
+        rgblight_sethsv(hue, sat, rgblight_get_val());
+        return false;
+    }
+
+    return true;
 }
